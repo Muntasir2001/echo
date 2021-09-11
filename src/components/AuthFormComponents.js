a054d971bfad48529a60a1ce4d20bdcc944446aa
Muntasir <muntasirenamalhuda@gmail.com>
2021-09-11 19:11:10 +0100

    restructered dir, created some folders

---
+++ src/components/AuthFormComponents.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const AuthForm = styled.form``;
+
+export const AuthFormHeading = styled.h1``;
+
+export const AuthFormInputDiv = styled.div``;
+
+export const AuthFormLabel = styled.label``;
+
+export const AuthFormInput = styled.input``;
+
+export const AuthFormSubmitDiv = styled.div``;
+
+export const AuthFormSubmitInput = styled.input``;
