30db9ef50e3893267dd8e8d144ce63badf09832b
Muntasir <muntasirenamalhuda@gmail.com>
2021-09-12 18:40:45 +0100

    did some stuff

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => {};
+import { Link, useHistory } from 'react-router-dom';
+
+import {
+	AuthForm,
+	AuthFormHeading,
+	AuthFormInputDiv,
+	AuthFormLabel,
+	AuthFormInput,
+	AuthFormSubmitDiv,
+	AuthFormSubmitInput,
+} from '../components/AuthFormComponents';
+
+const Login = () => {
+	const [email, setEmail] = useState({ email: '' });
+	const [password, setPassword] = useState({ password: '' });
+	const [error, setError] = useState(false);
+	const [errorMssg, setErrorMssg] = useState('');
+	const [loading, setLoading] = useState(false);
+
+	return (
+		<>
+			<h2>hello</h2>
+		</>
+	);
+};
 
 export default Login;
